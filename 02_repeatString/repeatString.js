d9f4a8951238086af01166fddcc342afb16fb8bd
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-15 12:06:54 +0530

    Add logic for repeating passed string count time
    Added the logic to the repeatString function to repeat the passed string
    the passed count no of time.
    Making changes accordingly for passing the test cases after removing the
    skip function call so that all of the defined test cases pass.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    if(count < 0) {
+        return "ERROR";
+    }
 
+    let repeatedString = "";
+
+    for (let i = 1; i <= count; i++) {
+        repeatedString += string;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line

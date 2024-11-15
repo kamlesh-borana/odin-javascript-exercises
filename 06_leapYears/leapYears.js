afb56ecaf8e55e687e3eacb482fb6d47bffe8d55
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-15 22:05:19 +0530

    Add logic for checking leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const isDivisible = (dividend, divisor) => {
+        return dividend % divisor === 0;
+    }
 
+    const isDivisibleBy4 = isDivisible(year, 4);
+    const isDivisibleBy100 = isDivisible(year, 100);
+    const isDivisibleBy400 = isDivisible(year, 400);
+
+    if(isDivisibleBy4 && (!isDivisibleBy100 || isDivisibleBy400)) {
+        return true;
+    }
+
+    return false;
 };
 
 // Do not edit below this line

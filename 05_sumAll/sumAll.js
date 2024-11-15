1c524a2041734230dfbafd4ff6c2ae489503f038
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-15 18:40:03 +0530

    Update sumAll to handle negative and non-integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,12 @@
 const sumAll = function(number1, number2) {
+    if(number1 < 0 || number2 < 0) {
+        return "ERROR";
+    }
+
+    if(!Number.isInteger(number1) || !Number.isInteger(number2)) {
+        return "ERROR";
+    }
+
     let sum = 0;
     let startNumber = number1 <= number2 ? number1 : number2;
     let endNumber = number1 <= number2 ? number2 : number1;

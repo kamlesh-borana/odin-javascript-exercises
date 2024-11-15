3526c6c2d3bd4780dec0cd5a7a440b80ae3dee07
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-15 22:59:42 +0530

    Add logic for fahrenheit and celsius conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureInFahrenheit) {
+  const temperatureInCelsius = (temperatureInFahrenheit - 32) / 1.8;
+
+  return Math.round(temperatureInCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureInCelsius) {
+  const temperatureInFahrenheit = (temperatureInCelsius * 1.8) + 32;
+
+  return Math.round(temperatureInFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line

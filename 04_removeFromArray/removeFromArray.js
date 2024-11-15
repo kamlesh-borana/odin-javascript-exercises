22f3ffa64f31e2bc6595de2d89381d76faa08cad
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-15 13:18:42 +0530

    Add logic to remove value from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value) {
+    const clonedArray = [...array];
+
+    const indexOfValue = clonedArray.indexOf(value);
+
+    // If value found inside the array delete it using the found index
+    if(indexOfValue > -1) {
+        clonedArray.splice(indexOfValue, 1);
+    }
+
+    return clonedArray;
 };
 
 // Do not edit below this line

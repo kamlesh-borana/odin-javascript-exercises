591a50dc18eebc02a2c8b7e90ae7f6cf0a9d98ba
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-15 18:28:34 +0530

    Update sumAll's logic to handle large number first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function(startNumber, endNumber) {
+const sumAll = function(number1, number2) {
     let sum = 0;
+    let startNumber = number1 <= number2 ? number1 : number2;
+    let endNumber = number1 <= number2 ? number2 : number1;
 
     for (let i = startNumber; i <= endNumber; i++) {
         sum += i;

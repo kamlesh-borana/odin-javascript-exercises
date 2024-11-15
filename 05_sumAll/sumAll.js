443b073865a1a3ee6f810986519d2cdffd0d44a2
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-15 18:16:11 +0530

    Add logic to sum all the range numbers passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(startNumber, endNumber) {
+    let sum = 0;
 
+    for (let i = startNumber; i <= endNumber; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

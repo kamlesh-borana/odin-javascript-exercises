a2827f2cd586fcf6461b2f847ec0b02cb047cc04
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-29 11:25:59 +0530

    Add logic for getting nth fibonacci number
    Added logic for getting the nth fibonacci number and handling all
    scenarios specified in test cases to pass all test.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if(number < 0) {
+        return "OOPS";
+    }
 
+    let fibonacciNumber = 0;
+    let firstPrevious = 0;
+    let secondPrevious = 0;
+
+    for(let i = 1; i <= number; i++) {
+        if(i === 1) {
+            fibonacciNumber = 1;
+        } else {
+            fibonacciNumber = firstPrevious + secondPrevious;
+        }
+        // fibonacciNumber = (firstPrevious + secondPrevious) || 1;
+        secondPrevious = firstPrevious;
+        firstPrevious = fibonacciNumber;
+    }
+
+    return fibonacciNumber;
 };
 
 // Do not edit below this line

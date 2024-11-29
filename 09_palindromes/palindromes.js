f6f04d9d96e8bd7de524260faef1a952d4c7f6a9
Kamlesh Borana <kamlesh.m.borana@gmail.com>
2024-11-29 10:19:56 +0530

    Add logic to check for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    for(let i = 1; i <= Math.floor(string.length / 2); i++) {
+        if(string[i - 1] !== string[string.length - i]) {
+            return false;
+        }
+    }
+    
+    return true;
 };
 
 // Do not edit below this line
